485ada180ef651efd743ccbd66f1d8eaf9962259
Aleksandra Lipert <ola.lipert@gmail.com>
2022-05-11 10:36:52 +0200

    Review changes

--- Carosa.Core/Database/DatabaseContext.cs
+++ Carosa.Core/Database/DatabaseContext.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Carosa.Core
+namespace Carosa.Core.Database
 {
-    public class _context : DbContext
+    public class DatabaseContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Data Source = localhost\SQLEXPRESS; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog = CarosaDB");
     }
