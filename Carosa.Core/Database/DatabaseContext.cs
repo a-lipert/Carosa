1072070127d152491dfcd92a6d5c0d92aa725c0a
Aleksandra Lipert <ola.lipert@gmail.com>
2022-05-11 14:48:00 +0200

    Adding customer entity

--- Carosa.Core/Database/DatabaseContext.cs
+++ Carosa.Core/Database/DatabaseContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Carosa.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Carosa.Core.Database
 {
     public class DatabaseContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Data Source = localhost\SQLEXPRESS; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog = CarosaDB");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Integrated Security=True; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=CarosaDB");
+
+        public DbSet<Customer> Customers { get; set; }
+
     }
+
+    
 }
