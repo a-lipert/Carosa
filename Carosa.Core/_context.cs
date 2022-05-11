969f6bdc74693d5d1462c4cd7a17f8414656966a
Aleksandra Lipert <ola.lipert@gmail.com>
2022-05-11 09:57:45 +0200

    Configured db connection

---
+++ Carosa.Core/_context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carosa.Core
+{
+    public class _context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Data Source = localhost\SQLEXPRESS; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog = CarosaDB");
+    }
+}
