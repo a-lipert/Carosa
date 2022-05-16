c14eb2f4844506722101db6b1090435e88940a99
Aleksandra Lipert <ola.lipert@gmail.com>
2022-05-16 13:58:24 +0200

    Delete suo file from repository

---
+++ Carosa.Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Carosa.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
