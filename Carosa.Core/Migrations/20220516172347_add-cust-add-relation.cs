adb20cab1a1820b21e6c2f6e8250020289ad6944
Aleksandra Lipert <ola.lipert@gmail.com>
2022-05-16 19:26:31 +0200

    Adding customer-address relation

---
+++ Carosa.Core/Migrations/20220516172347_add-cust-add-relation.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carosa.Core.Migrations
+{
+    public partial class addcustaddrelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HomeAddressId",
+                table: "Customers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "InvoiceAddressId",
+                table: "Customers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "InvoiceAddressSameAsHome",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_HomeAddressId",
+                table: "Customers",
+                column: "HomeAddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_InvoiceAddressId",
+                table: "Customers",
+                column: "InvoiceAddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Address_HomeAddressId",
+                table: "Customers",
+                column: "HomeAddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Address_InvoiceAddressId",
+                table: "Customers",
+                column: "InvoiceAddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Address_HomeAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Address_InvoiceAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_HomeAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_InvoiceAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "HomeAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceAddressSameAsHome",
+                table: "Customers");
+        }
+    }
+}
--- Carosa.Entities/Customer.cs
+++ Carosa.Entities/Customer.cs
@@ -6,6 +6,10 @@
         public string LastName { get; set; }
         public int PhoneNumber { get; set; }
         public string Email { get; set; }
-
+        public Address InvoiceAddress { get; set; }
+        public int? InvoiceAddressId { get; set; }
+        public Address HomeAddress { get; set; }
+        public int? HomeAddressId { get; set; }
+        public bool InvoiceAddressSameAsHome { get; set; }
     }
 }
