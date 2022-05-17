de4d80859fe391b1dd291bee98ff790987f3ba10
Aleksandra Lipert <ola.lipert@gmail.com>
2022-05-17 10:05:13 +0200

    Adding VehicleBrand entity

---
+++ Carosa.Entities/VehicleBrand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carosa.Entities
+{
+    public class VehicleBrand : BaseEntity
+    {
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        [StringLength(3)]
+        public string CountryOfOriginIso { get; set; }
+    }
+}
