d934c52deca453b4dc95f1f11e97fe7cedf93d4d
Aleksandra Lipert <ola.lipert@gmail.com>
2022-05-18 09:59:00 +0200

    Add VehicleEntity

---
+++ Carosa.Entities/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿namespace Carosa.Entities
+{
+    public class Vehicle : BaseEntity
+    {
+        public enum Type
+        {
+            Car,
+            Bicycle,
+            Motorbike,
+            EScooter
+        }
+        public int VehicleBrandId { get; set; }
+        public VehicleBrand VehicleBrand { get; set; }
+        public int MaxSpeed { get; set; }
+        public int ServiceAfterKm { get; set; }
+        public decimal PriceWhenNew { get; set; }
+        public decimal UsagePrice { get; set; }
+        public enum UsagePriceType
+        {
+            Kilometers,
+            Minutes
+        }
+    }
+}
